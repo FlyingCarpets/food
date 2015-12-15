6e18b897bcbae7ba156ad8eff96680696f03b329
ZigmasBKatinas <ritab@RitaBs-MacBook.local>
2015-12-15 17:01:36 +0200

    additional

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	//alert("Busy");
+
+	//accordion
+
+	$("#accordion").fadeIn("slow");
+	$( "#accordion" ).accordion({
+		event: "click",
+        active: false,
+        collapsible: true,
+    });
+
+	
+
+	//adipoli
+    $('.row1').adipoli({
+		'startEffect' : 'normal',
+		'hoverEffect' : 'popout'
+	});
+	overlay = $("#overlay");
+	img = $("#myimg");
+	overlay.width(img.css("width"));
+	overlay.height(img.css("height"));
+	overlay.css("top", img.offset().top + "px");
+	overlay.css("left", img.offset().left + "px");
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000
+    });
+});
+
+
+
+
+
+
